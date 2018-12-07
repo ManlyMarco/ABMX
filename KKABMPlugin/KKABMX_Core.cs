4c7e27da65831d0d9c206e0caf2475e7c7af89e5
ManlyMarco <dummyaccx@gmail.com>
2018-12-07 22:20:01 +0100

    Version up to 2.2

--- KKABMPlugin/KKABMX_Core.cs
+++ KKABMPlugin/KKABMX_Core.cs
@@ -6,7 +6,7 @@ namespace KKABMX.Core
     [BepInDependency("com.bepis.bepinex.extendedsave")]
     public class KKABMX_Core : BaseUnityPlugin
     {
-        public const string Version = "2.1";
+        public const string Version = "2.2";
         public const string GUID = "KKABMX.Core";
 
         public KKABMX_Core()
