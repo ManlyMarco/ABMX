e0f46b4b053fa2063e7ac85cb4fdcd9caea40f22
ManlyMarco <dummyaccx@gmail.com>
2019-03-20 21:44:40 +0100

    Refactor; version up to 3.0.1

---
+++ KKABMPlugin/KKABMX_Core.Hooks.cs
@@ -0,0 +1,30 @@
+﻿using Harmony;
+using KKAPI.Maker;
+using Studio;
+
+namespace KKABMX.Core
+{
+    public partial class KKABMX_Core
+    {
+        private static class Hooks
+        {
+            [HarmonyPostfix, HarmonyPatch(typeof(ShapeInfoBase), nameof(ShapeInfoBase.ChangeValue))]
+            public static void ChangeValuePost(bool __result)
+            {
+                if (!__result) return;
+
+                var controller = MakerAPI.GetCharacterControl()?.GetComponent<BoneController>();
+                if (controller != null)
+                    controller.NeedsBaselineUpdate = true;
+            }
+
+            [HarmonyPostfix, HarmonyPatch(typeof(OCIChar), nameof(OCIChar.ActiveKinematicMode))]
+            public static void ActiveKinematicModePost(OCIChar __instance)
+            {
+                var controller = __instance.charInfo?.GetComponent<BoneController>();
+                if (controller != null)
+                    controller.NeedsFullRefresh = true;
+            }
+        }
+    }
+}
--- KKABMPlugin/KKABMX_Core.cs
+++ KKABMPlugin/KKABMX_Core.cs
@@ -1,16 +1,14 @@
 ﻿using BepInEx;
 using Harmony;
 using KKAPI.Chara;
-using KKAPI.Maker;
-using Studio;
 
 namespace KKABMX.Core
 {
     [BepInPlugin(GUID, "KKABMX Core", Version)]
     [BepInDependency("com.bepis.bepinex.extendedsave")]
-    public class KKABMX_Core : BaseUnityPlugin
+    public partial class KKABMX_Core : BaseUnityPlugin
     {
-        internal const string Version = "3.0";
+        internal const string Version = "3.0.1";
         public const string GUID = "KKABMX.Core";
         public const string ExtDataGUID = "KKABMPlugin.ABMData";
 
@@ -22,27 +20,6 @@ namespace KKABMX.Core
             CharacterApi.RegisterExtraBehaviour<BoneController>(ExtDataGUID);
 
             HarmonyInstance.Create(GUID).PatchAll(typeof(Hooks));
-        }
-
-        private static class Hooks
-        {
-            [HarmonyPostfix, HarmonyPatch(typeof(ShapeInfoBase), nameof(ShapeInfoBase.ChangeValue))]
-            public static void ChangeValuePost(bool __result)
-            {
-                if (!__result) return;
-
-                var controller = MakerAPI.GetCharacterControl()?.GetComponent<BoneController>();
-                if (controller != null)
-                    controller.NeedsBaselineUpdate = true;
-            }
-
-            [HarmonyPostfix, HarmonyPatch(typeof(OCIChar), nameof(OCIChar.ActiveKinematicMode))]
-            public static void ActiveKinematicModePost(OCIChar __instance)
-            {
-                var controller = __instance.charInfo?.GetComponent<BoneController>();
-                if (controller != null)
-                    controller.NeedsFullRefresh = true;
-            }
         }
     }
 }
