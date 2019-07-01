c345e32c129460986a94e2f037f3814ba2860077
ManlyMarco <dummyaccx@gmail.com>
2019-07-01 17:33:48 +0200

    Version up to 3.2.1

--- ABMX/Metadata.cs
+++ ABMX/Metadata.cs
@@ -2,7 +2,7 @@
 {
     internal static class Metadata
     {
-        public const string Version = "3.2";
+        public const string Version = "3.2.1";
         public const string GUID = "KKABMX.Core";
         public const string ExtDataGUID = "KKABMPlugin.ABMData";
     }
