fdba65a37b5402963b4f0cccbb91f5f6a8703f41
ManlyMarco <dummyaccx@gmail.com>
2020-04-25 00:46:09 +0200

    Fix importing old character data in studio

--- PHABMX/Core/Legacy/OldDataConverter.cs
+++ PHABMX/Core/Legacy/OldDataConverter.cs
@@ -4,25 +4,31 @@ using System.Linq;
 using KKAPI.Studio;
 using UnityEngine;
 using MessagePack;
+using Studio;
 
 namespace KKABMX.Core
 {
     internal static class OldDataConverter
     {
+        private static string GetCharacterName(this Human h)
+        {
+            if (StudioAPI.InsideStudio)
+            {
+                var charaName = Studio.Studio.Instance.dicObjectCtrl.Values.OfType<OCIChar>().FirstOrDefault(x => x.charInfo.human == h)?.charStatus.name;
+                if (!string.IsNullOrEmpty(charaName)) return charaName;
+            }
+            return h is Female f
+                ? Female.HeroineName(f.HeroineID)
+                : Male.MaleName(((Male)h).MaleID);
+        }
+
         public static List<BoneModifier> ImportOldData(string cardPath, Human human)
         {
             Dictionary<int, BoneModHarmony.BoneModifier> modifiers;
 
             var sex = human.sex;
 
-            string charaName = null;
-            if (StudioAPI.InsideStudio)
-                charaName = StudioAPI.GetSelectedCharacters().FirstOrDefault(x => x.charInfo.human == human)
-                    ?.charStatus.name;
-            if (string.IsNullOrEmpty(charaName))
-                charaName = human is Female f
-                    ? Female.HeroineName(f.HeroineID)
-                    : Male.MaleName(((Male)human).MaleID);
+            var charaName = GetCharacterName(human);
 
             string path = null;
             if (!string.IsNullOrEmpty(cardPath))
