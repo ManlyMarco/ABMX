b86b6b1f686bbd172d2df1f2d0f88d4587de99bd
ManlyMarco <dummyaccx@gmail.com>
2020-04-25 00:46:56 +0200

    Prevent from running if BMM is detected; studio tweaks

--- PHABMX/Core/KKABMX_Core.cs
+++ PHABMX/Core/KKABMX_Core.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using BepInEx;
 using BepInEx.Configuration;
@@ -8,6 +10,7 @@ using KKABMX.GUI;
 using KKAPI;
 using KKAPI.Chara;
 using KKAPI.Maker;
+using KKAPI.Studio;
 using UnityEngine;
 
 namespace KKABMX.Core
@@ -30,20 +33,33 @@ namespace KKABMX.Core
         internal static new ManualLogSource Logger { get; private set; }
         private ConfigEntry<KeyboardShortcut> _openEditorKey;
 
-        private void Start()
+        private IEnumerator Start()
         {
             Instance = this;
             Logger = base.Logger;
 
-            gameObject.AddComponent<KKABMX_AdvancedGUI>();
-
             XyzMode = Config.Bind("Maker", Metadata.XyzModeName, false, Metadata.XyzModeDesc);
             RaiseLimits = Config.Bind("Maker", Metadata.RaiseLimitsName, false, Metadata.RaiseLimitsDesc);
             TransparentAdvancedWindow = Config.Bind("General", Metadata.AdvTransparencyName, false, Metadata.AdvTransparencyDesc);
-            _openEditorKey = Config.Bind("General", "Open bonemod editor", KeyboardShortcut.Empty, "Opens advanced bonemod window if there is a character that can be edited.");
+            // Use key B to be an in-place replacement for bonemodharmony
+            _openEditorKey = Config.Bind("General", "Open bonemod editor", new KeyboardShortcut(KeyCode.B), "Opens advanced bonemod window if there is a character that can be edited.");
 
-            if (KKAPI.Studio.StudioAPI.InsideStudio)
+            // Wait for BMM to get loaded
+            yield return null;
+
+            var bmmExists = Type.GetType("BoneModHarmony.BMMHuman, BoneModHarmony") != null;
+            if (bmmExists)
             {
+                Logger.Log(LogLevel.Error | LogLevel.Message, "PHABMX can't load because it is incompatible with BoneModHarmony. Remove BoneModHarmony and try again.");
+                enabled = false;
+                yield break;
+            }
+
+            gameObject.AddComponent<KKABMX_AdvancedGUI>();
+
+            if (StudioAPI.InsideStudio)
+            {
+                //todo
                 //KKAPI.Studio.StudioAPI.GetOrCreateCurrentStateCategory(null)
                 //    .AddControl(new KKAPI.Studio.UI.CurrentStateCategorySwitch("Show Bonemod", c => false))
                 //    .Value.Subscribe(show =>
@@ -75,14 +91,21 @@ namespace KKABMX.Core
                 {
                     var g = GetCurrentVisibleGirl();
                     if (g != null)
+                    {
                         KKABMX_AdvancedGUI.Enable(g.GetComponent<BoneController>());
+                    }
                     else
-                        Logger.LogMessage("No characters found to edit");
+                    {
+                        if (StudioAPI.InsideStudio)
+                            Logger.LogMessage("No characters selected. Select a character to edit its bones.");
+                        else
+                            Logger.LogMessage("No characters found to edit");
+                    }
                 }
             }
         }
 
-        private Human GetCurrentVisibleGirl()
+        private static Human GetCurrentVisibleGirl()
         {
             if (MakerAPI.InsideMaker)
                 return MakerAPI.GetCharacterControl();
@@ -91,9 +114,9 @@ namespace KKABMX.Core
             var f = m?.GetComponentInChildren<Female>();
             if (f != null) return f;
 
-            if (KKAPI.Studio.StudioAPI.InsideStudio)
+            if (StudioAPI.InsideStudio)
             {
-                var c = KKAPI.Studio.StudioAPI.GetSelectedControllers<BoneController>().FirstOrDefault();
+                var c = StudioAPI.GetSelectedControllers<BoneController>().FirstOrDefault();
                 return c?.ChaControl;
             }
 
