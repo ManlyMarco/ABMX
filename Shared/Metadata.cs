31ee77ea5f25d0be3965a8a86b8b6776395d93a7
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2021-11-26 20:06:56 +0100

    Version up to 4.4.6

--- Shared/Metadata.cs
+++ Shared/Metadata.cs
@@ -2,7 +2,7 @@
 {
     internal static class Metadata
     {
-        public const string Version = "4.4.5";
+        public const string Version = "4.4.6";
 #if AI
         public const string Name = "AIABMX (BonemodX)";
 #elif HS2
