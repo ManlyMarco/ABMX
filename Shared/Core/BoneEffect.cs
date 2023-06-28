9a3a1ea4dacc57f999124766da6cfcda15afae31
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-06-28 22:51:29 +0200

    Update BoneEffect.cs
    Clarify BoneEffect xmldocs

--- Shared/Core/BoneEffect.cs
+++ Shared/Core/BoneEffect.cs
@@ -14,7 +14,9 @@ namespace KKABMX.Core
     public abstract class BoneEffect
     {
         /// <summary>
-        /// Should return names of all bones that can be affected by this effect. Only called when reloading/rebuilding modifiers.
+        /// Should return names of all bones that can be affected by this effect.
+        /// It's recommended to store the bone list in a static array and simply return that array.
+        /// This may be called on every frame for each character, should be fast.
         /// </summary>
         /// <param name="origin">Bone controller that this effect applies to</param>
         public abstract IEnumerable<string> GetAffectedBones(BoneController origin);
@@ -22,7 +24,7 @@ namespace KKABMX.Core
         /// <summary>
         /// Get effect for the specified bone. If no effect should be applied, null should be returned.
         /// The modifier will be multiplied with other modifiers used on this bone, so 1 is the no-change value.
-        /// Called every time for each character, should be fast.
+        /// This is called on every frame for each character, should be fast.
         /// </summary>
         /// <param name="bone">Bone to get effect for</param>
         /// <param name="origin">Bone controller that this effect applies to</param>
