f8f89e6a69366cf814e5e7a80d04e3f4a82bd955
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-12-04 13:51:50 +0100

    Version up to 5.1

--- Shared/Metadata.cs
+++ Shared/Metadata.cs
@@ -4,7 +4,7 @@ namespace KKABMX
 {
     internal static class Metadata
     {
-        public const string Version = "5.0.6";
+        public const string Version = "5.1";
 #if AI
         public const string Name = "AIABMX (BonemodX)";
 #elif HS2
